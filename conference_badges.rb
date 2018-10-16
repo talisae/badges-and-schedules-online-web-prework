3c87dc2018c4f5e8e52c686907da36a84970193b
Talisa Erazo <talisaerazo@gmail.com>
2018-10-16 23:34:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,3 +21,13 @@ def assign_rooms(array)
   end
   return newArray
 end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+
+  assign_rooms(array).each do |id|
+    puts id
+  end
+end
